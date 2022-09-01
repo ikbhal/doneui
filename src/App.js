2a54ae5d8dda3301ef02db50b915aca5b8e5a472
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2022-09-01 09:20:38 +0530

    loading, toggle connect with backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  var [isLoading, setLoading] = useState(true);
+  var [name, setName] = useState('Jigs Store');
+  var [isDone, setDone] = useState(false);
+
+  useEffect(()=>{
+    axios({
+      // Endpoint to send files
+      url: "http://localhost:8000/items/1",
+      method: "GET",
+    })
+      // Handle the response from backend here
+    .then((res) => {
+
+        console.log(res);
+        console.log("res.data", res.data);
+        var item = res.data;
+        setName(item.name);
+        setDone(item.isDone);
+        setLoading(false);
+    })
+    // Catch errors if any
+    .catch((err) => {
+        console.error(err);
+    });
+  },
+  []);
+  var toggleDone = (e)=>{
+    console.log("toggle done");
+    setDone(!isDone);
+    axios({
+
+      // Endpoint to send files
+      url: "http://localhost:8000/items/1/toggledone",
+      method: "PUT",
+    })
+      // Handle the response from backend here
+      .then((res) => { console.log(res)})
+      // Catch errors if any
+      .catch((err) => { console.error(err)});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Done</h1>
+      {isLoading ? '<p>Loading ..<.p>':
+        <div className="item">
+          <input type="checkbox" checked={isDone}
+            onChange = {toggleDone}
+            />
+          {name} <br/>
+        </div>
+      }
     </div>
   );
 }
